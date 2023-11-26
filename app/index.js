4c2cd550cd5ed4caf57cf422701c7fd176cba46b
akashGangwani <akash.gangwani1998@gmail.com>
2023-11-26 19:30:53 +0530

    added both route details in highlightedPlaces json

--- app/index.js
+++ app/index.js
@@ -1,52 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapView,{ Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { useNavigation } from 'expo-router';
-
+import { highlightedPlaces, route1Places, route2Places } from '../highlightedPlaces';
+import MapViewDirections from 'react-native-maps-directions';
+import app from '../app.json'
 
 export default function Page() {
 const nav=useNavigation();
-  const touristSpots = [
-    {
-      id: 1,
-      title: "TCS Powai",
-      coordinates: { latitude: 19.1197, longitude: 72.9051 },
-      description: `Embrace the cliche and opt for a horse and carriage ride departing from the Gateway at dusk - it's a lovely time to tour the city.
-      
-      Created to mark the illustrious visit of King George V and Queen Mary in 1911, the Gateway of India was not completed until 1948, two years before India's independence from England. Instead, the British royals landed on Indian soil to find a cardboard cutout fashioned to show the intended finished product.
+ const [touristSpots,setTouristSpot]=useState(route2Places);
 
-      Though it was constructed to welcome the royal couple, the Indo-Saracenic structure has since become known as the place where the British bid their final adieu to the Subcontinent following the end of the Raj.
-      
-      These days, the 26 metre arch facing Mumbai's harbor acts as a meeting place for locals and travellers alike. Boats for Elephanta Island depart from under the arch, while sweet vendors and family photographers ply the square 24/7.
-      
-      Created to mark the illustrious visit of King George V and Queen Mary in 1911, the Gateway of India was not completed until 1948, two years before India's independence from England. Instead, the British royals landed on Indian soil to find a cardboard cutout fashioned to show the intended finished product.
 
-      Though it was constructed to welcome the royal couple, the Indo-Saracenic structure has since become known as the place where the British bid their final adieu to the Subcontinent following the end of the Raj.`,
-      images: [
-        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
-        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
-        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
-      ],
-    },
-    {
-      id: 2,
-      title: "Gateway of India",
-      coordinates: { latitude: 18.9217, longitude: 72.8347 },
-      description: `Embrace the cliche and opt for a horse and carriage ride departing from the Gateway at dusk - it's a lovely time to tour the city.
-      
-      Created to mark the illustrious visit of King George V and Queen Mary in 1911, the Gateway of India was not completed until 1948, two years before India's independence from England. Instead, the British royals landed on Indian soil to find a cardboard cutout fashioned to show the intended finished product.
+ const [destination, setDestination] = useState(touristSpots[touristSpots.length-1].coordinates);
 
-      Though it was constructed to welcome the royal couple, the Indo-Saracenic structure has since become known as the place where the British bid their final adieu to the Subcontinent following the end of the Raj.
-      
-      These days, the 26 metre arch facing Mumbai's harbor acts as a meeting place for locals and travellers alike. Boats for Elephanta Island depart from under the arch, while sweet vendors and family photographers ply the square 24/7.`,
-      images: [
-        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
-        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
-        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
-      ],
-    },
-  ];
-
+const [origin, setOrigin] = useState(touristSpots[0].coordinates);
 
   return (
     <View style={styles.container}>
@@ -59,6 +26,14 @@ const nav=useNavigation();
         longitudeDelta: 0.0421,
       }}
       >
+           <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={"AIzaSyB77YshREiafXEk_-zvPM55DdnYpzbelak"}
+          strokeWidth={4}
+          strokeColor="blue"
+        />
+        
   {touristSpots?.map((spot) => (
           <Marker
             key={spot.id}
---
+++ highlightedPlaces.js
@@ -0,0 +1,214 @@
+export const highlightedPlaces=[
+  {
+    id: 1,
+    title: "TCS Powai",
+    coordinates: { latitude: 19.1197, longitude: 72.9051 },
+    description: `Embrace the cliche and opt for a horse and carriage ride departing from the Gateway at dusk - it's a lovely time to tour the city.
+    
+    Created to mark the illustrious visit of King George V and Queen Mary in 1911, the Gateway of India was not completed until 1948, two years before India's independence from England. Instead, the British royals landed on Indian soil to find a cardboard cutout fashioned to show the intended finished product.
+
+    Though it was constructed to welcome the royal couple, the Indo-Saracenic structure has since become known as the place where the British bid their final adieu to the Subcontinent following the end of the Raj.
+    
+    These days, the 26 metre arch facing Mumbai's harbor acts as a meeting place for locals and travellers alike. Boats for Elephanta Island depart from under the arch, while sweet vendors and family photographers ply the square 24/7.
+    
+    Created to mark the illustrious visit of King George V and Queen Mary in 1911, the Gateway of India was not completed until 1948, two years before India's independence from England. Instead, the British royals landed on Indian soil to find a cardboard cutout fashioned to show the intended finished product.
+
+    Though it was constructed to welcome the royal couple, the Indo-Saracenic structure has since become known as the place where the British bid their final adieu to the Subcontinent following the end of the Raj.`,
+    images: [
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
+    ],
+  },
+  {
+    "id": 2,
+    "title": "Gateway of India",
+    "coordinates": { latitude: 18.9217, longitude: 72.8347 },
+    description: `Embrace the cliche and opt for a horse and carriage ride departing from the Gateway at dusk - it's a lovely time to tour the city.
+    
+    Created to mark the illustrious visit of King George V and Queen Mary in 1911, the Gateway of India was not completed until 1948, two years before India's independence from England. Instead, the British royals landed on Indian soil to find a cardboard cutout fashioned to show the intended finished product.
+
+    Though it was constructed to welcome the royal couple, the Indo-Saracenic structure has since become known as the place where the British bid their final adieu to the Subcontinent following the end of the Raj.
+    
+    These days, the 26 metre arch facing Mumbai's harbor acts as a meeting place for locals and travellers alike. Boats for Elephanta Island depart from under the arch, while sweet vendors and family photographers ply the square 24/7.`,
+    images: [
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201812/Historical_Gateway_Of_India.jpeg?size=690:388",
+    ],
+  },
+]
+
+export const route1Places=[
+  {
+    "id": 1,
+    "title": "Taj Hotel",
+    "coordinates": {
+      "latitude": 18.92168,
+      "longitude": 72.833212
+    },
+    "description": `The Taj Hotel, located in the heart of Mumbai, stands as an emblem of opulence and historical significance. Nestled along the Arabian Sea, this iconic structure is a testament to India's rich architectural and cultural heritage. Completed in 1903, the hotel exudes an aura of regal grandeur, blending Moorish, Oriental, and Florentine styles seamlessly.
+
+    With its renowned Gateway of India as a backdrop, the Taj Hotel has played witness to decades of Mumbai's evolution. Its imposing red-domed architecture and intricate detailing captivate visitors, offering a glimpse into a bygone era of elegance and refinement. The Taj has been a silent witness to India's history, hosting dignitaries, celebrities, and world leaders.
+    
+    Beyond its architectural splendor, the Taj is celebrated for its impeccable service and luxurious accommodations. The hotel's hospitality is synonymous with excellence, creating an experience that transcends mere accommodation. From its sumptuous dining options to its lush gardens and panoramic views, the Taj Hotel is a living testament to the fusion of tradition and modernity, making it an enduring symbol of India's cultural tapestry and a cherished destination for those seeking an extraordinary retreat.`,
+     "images": ["https://th.bing.com/th/id/R.68363d336064ef5800533240bfa0d981?rik=WgFNIp9SsFa4vA&riu=http%3a%2f%2f3.bp.blogspot.com%2f-Mny9xrIo_Yk%2fUpWCi1kkgoI%2fAAAAAAAANak%2fh9gH8p6BZX8%2fs1600%2fTaj%2bMahal%2bhotel.jpg&ehk=E0q369l%2f3RglRzAYcSiKYjll9pF%2fqq1aCNyOna18xTA%3d&risl=&pid=ImgRaw&r=0",
+ "https://pix10.agoda.net/hotelImages/109/109719/109719_14072911540020544350.jpg?s=1024x768",
+     "https://th.bing.com/th/id/OIP.KnGX-2xA28Pb8KHCJw0oAAHaDP?rs=1&pid=ImgDetMain"
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Royal Bombay Yacht Club",
+    "coordinates": {
+      "latitude": 18.92352,
+      "longitude": 72.83349
+    },
+    "description": "The Royal Bombay Yacht Club, founded in 1846, inertwines Mumbai's rick maritime history, colonial-era architectural splendor, and vibrant yachting culture, offering exclusive membership to a community of sea enthusiasts while preserving its heritage as a gateway to maritime exploration.",
+    "images": ["royal_yacht_club_image1.jpg", "royal_yacht_club_image2.jpg"]
+  },
+  {
+    "id": 3,
+    "title": "Dhanraj Mahal",
+    "coordinates": {
+      "latitude": 18.92380,
+      "longitude": 72.8340
+    },
+    "description": "Historical building",
+    "images": ["dhanraj_mahal_image1.jpg", "dhanraj_mahal_image2.jpg"]
+  },
+  {
+    "id": 4,
+    "title": "Naval Dockyard",
+    "coordinates": {
+      "latitude": 18.9263,
+      "longitude": 72.8343
+    },
+    "description": "Military shipyard",
+    "images": ["naval_dockyard_image1.jpg", "naval_dockyard_image2.jpg"]
+  },
+  {
+    "id": 5,
+    "title": "Asiatic Library",
+    "coordinates": {
+      "latitude": 18.93190,
+      "longitude": 72.835859
+    },
+    "description": "Historical library",
+    "images": ["asiatic_library_image1.jpg", "asiatic_library_image2.jpg"]
+  },
+  {
+    "id": 6,
+    "title": "Reserve Bank Of India",
+    "coordinates": {
+      "latitude": 18.933282,
+      "longitude": 72.837300
+    },
+    "description": "Central bank",
+    "images": ["rbi_image1.jpg", "rbi_image2.jpg"]
+  },
+  {
+    "id": 7,
+    "title": "Mazagaon Docks",
+    "coordinates": {
+      "latitude": 18.9656,
+      "longitude": 72.8469
+    },
+    "description": "Shipbuilding company",
+    "images": ["mazagaon_docks_image1.jpg", "mazagaon_docks_image2.jpg"]
+  },
+  {
+    "id": 8,
+    "title": "Rashtriya Chemical and Fertilizers Limited (RCF)",
+    "coordinates": {
+      "latitude": 19.03467,
+      "longitude": 72.891789
+    },
+    "description": "Chemical and fertilizer company",
+    "images": ["rcf_image1.jpg", "rcf_image2.jpg"]
+  },
+  {
+    "id": 9,
+    "title": "TCS Powai",
+    "coordinates": {
+      "latitude": 19.1117,
+      "longitude": 72.9105
+    },
+    "description": "Tata Consultancy Services campus",
+    "images": ["tcs_powai_image1.jpg", "tcs_powai_image2.jpg"]
+  }
+]
+
+
+export const route2Places=[
+  {
+    "id": 10,
+    "title": "TCS, Banyan Park, Mumbai",
+    "coordinates": {
+      "latitude": 19.120814,
+      "longitude": 72.857302
+    },
+    "description": "Serene park with a historic banyan tree",
+    "images": ["banyan_park_image1.jpg", "banyan_park_image2.jpg"]
+  },
+  {
+    "id": 11,
+    "title": "Bandra Worli Sea-link",
+    "coordinates": {
+      "latitude": 19.029862,
+      "longitude": 72.815752
+    },
+    "description": "Architectural marvel connecting Bandra and Worli",
+    "images": ["bandra_worli_sealink_image1.jpg", "bandra_worli_sealink_image2.jpg"]
+  },
+  {
+    "id": 12,
+    "title": "Haji Ali Dargah",
+    "coordinates": {
+      "latitude": 18.982167,
+      "longitude": 72.808907
+    },
+    "description": "Historic mosque and tomb on an islet",
+    "images": ["haji_ali_dargah_image1.jpg", "haji_ali_dargah_image2.jpg"]
+  },
+  {
+    "id": 13,
+    "title": "Antilia",
+    "coordinates": {
+      "latitude": 18.968074,
+      "longitude": 72.809495
+    },
+    "description": "Residence of Mukesh Ambani, one of the world's most expensive homes",
+    "images": ["antilia_image1.jpg", "antilia_image2.jpg"]
+  },
+  {
+    "id": 14,
+    "title": "Marine Drive",
+    "coordinates": {
+      "latitude": 18.955540,
+      "longitude": 72.812550
+    },
+    "description": "Iconic boulevard along the Arabian Sea",
+    "images": ["marine_drive_image1.jpg", "marine_drive_image2.jpg"]
+  },
+  {
+    "id": 15,
+    "title": "Art Deco Building (near Marine Drive and Oval Maidan)",
+    "coordinates": {
+      "latitude": 18.930423,
+      "longitude": 72.834386
+    },
+    "description": "Exquisite example of Art Deco architecture",
+    "images": ["art_deco_building_image1.jpg", "art_deco_building_image2.jpg"]
+  },
+  {
+    "id": 16,
+    "title": "Taj Hotel",
+    "coordinates":  {
+      "latitude": 18.92168,
+      "longitude": 72.833212
+    },
+    "description": "Iconic luxury hotel",
+    "images": ["taj_hotel_image1.jpg", "taj_hotel_image2.jpg"]
+  }
+]
